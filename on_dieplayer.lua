31d2af0ea3c6b80d137610020aaa2ba020526d60
flux <25628292+fluxionary@users.noreply.github.com>
2022-10-01 10:01:42 -0700

    add some checks to prevent bone placement if something weird happens

--- on_dieplayer.lua
+++ on_dieplayer.lua
@@ -10,7 +10,23 @@ local keep_on_failure = settings.keep_on_failure
 local search_distance = settings.search_distance
 
 function api.on_dieplayer(player)
+	if not minetest.is_player(player) then
+		bones.log("error", "non-player died: %q", dump(player))
+		return
+	end
+
 	local player_name = player:get_player_name()
+
+	if (not player_name) or player_name == "" then
+		bones.log("error", "player has no name? %q", dump(player))
+		return
+	end
+
+	if not player:get_pos() then
+		bones.log("error", "player has no position? %q", dump(player))
+		return
+	end
+
 	local death_pos = api.get_death_pos(player)
 
 	local mode = minetest.is_protected(death_pos, player_name) and mode_protected or bones_mode
